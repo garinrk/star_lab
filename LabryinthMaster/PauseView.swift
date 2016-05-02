a90f567e4f6fa95d6a275166df7c5c5ac1bcc7ca
jmbiggs <eraserhead@gmail.com>
2016-05-02 17:35:17 -0600

    started on pause view

--- LabryinthMaster/PauseView.swift
+++ LabryinthMaster/PauseView.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class PauseView : UIView{
+    var resumeButton = UIButton(type: UIButtonType.Custom)
+    var quitButton = UIButton(type: UIButtonType.Custom)
+    var musicSlider : UISlider?
+    var fxSlider : UISlider?
+    
     var musicLabel = UILabel()
     var fxLabel = UILabel()
     var screenRect = UIScreen.mainScreen().bounds
@@ -36,41 +41,36 @@ class PauseView : UIView{
     var _gameManager : GameManager = GameManager.sharedInstance
     
     override func drawRect(rect: CGRect) {
-    
         
-//        pauseTitleLabel.frame = CGRectMake(CGRectGetMidX(rect), CGRectGetMidY(rect), 200, 50)
-//        pauseTitleLabel.text = "Pause Menu"
-//        pauseTitleLabel.textColor = UIColor.blueColor()
-//        pauseTitleLabel.adjustsFontSizeToFitWidth = true
-//        pauseTitleLabel.center = CGPointMake(CGRectGetMidX(self.frame),CGRectGetMaxY(self.frame) * 0.25)
-//        pauseTitleLabel.textAlignment = .Center
+        resumeButton.setTitle("Resume Game",forState: UIControlState.Normal)
+        resumeButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        resumeButton.layer.borderWidth = 5
+        resumeButton.backgroundColor = UIColor(white: 0, alpha: 0.5)
+        resumeButton.layer.cornerRadius = 5
+        resumeButton.layer.borderColor = UIColor.whiteColor().CGColor
+        addSubview(resumeButton)
+        resumeButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        quitButton.setTitle("Quit Game", forState: UIControlState.Normal)
+        quitButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        quitButton.layer.borderWidth = 5
+        quitButton.backgroundColor = UIColor(white: 0, alpha: 0.5)
+        quitButton.layer.cornerRadius = 5
+        quitButton.layer.borderColor = UIColor.whiteColor().CGColor
+        addSubview(quitButton)
+        
         titleImageBackground.frame = CGRectMake(CGRectGetMidX(rect), CGRectGetMidY(rect), 200, 50)
         titleImageBackground.image = titleImage
         self.addSubview(titleImageBackground)
         
-//        musicLabel.frame = CGRectMake(CGRectGetMidX(rect), CGRectGetMidY(rect), 200, 50)
-//        musicLabel.text = "Music Level"
-//        musicLabel.adjustsFontSizeToFitWidth = true
-//        musicLabel.center = CGPointMake(CGRectGetMaxX(self.frame) * 0.30,CGRectGetMaxY(self.frame) * 0.55)
-//        musicLabel.textAlignment = .Center
         musicLevelImageBackground.frame = CGRectMake(CGRectGetMidX(rect), CGRectGetMidY(rect), 200, 50)
         musicLevelImageBackground.image = musicLevelImage
         self.addSubview(musicLevelImageBackground)
         
-//        fxLabel.frame = CGRectMake(CGRectGetMidX(rect), CGRectGetMidY(rect), 200, 50)
-//        fxLabel.text = "Effects Level"
-//        fxLabel.adjustsFontSizeToFitWidth = true
-//        fxLabel.center = CGPointMake(CGRectGetMaxX(self.frame) * 0.30,CGRectGetMaxY(self.frame) * 0.60)
-//        fxLabel.textAlignment = .Center
         fxLevelImageBackground.frame = CGRectMake(CGRectGetMidX(rect), CGRectGetMidY(rect), 200, 50)
         fxLevelImageBackground.image = fxLevelImage
         self.addSubview(fxLevelImageBackground)
         
-//        currentScoreLabel.frame = CGRectMake(CGRectGetMidX(rect), CGRectGetMidY(rect), 200, 50)
-//        currentScoreLabel.text = "Current Score"
-//        currentScoreLabel.adjustsFontSizeToFitWidth = true
-//        currentScoreLabel.center = CGPointMake(CGRectGetMaxX(self.frame) * 0.30,CGRectGetMaxY(self.frame) * 0.70)
-//        currentScoreLabel.textAlignment = .Center
         currentScoreImageBackground.frame = CGRectMake(CGRectGetMidX(rect), CGRectGetMidY(rect), 200, 50)
         currentScoreImageBackground.image = currentScoreImage
         self.addSubview(currentScoreImageBackground)
@@ -81,11 +81,6 @@ class PauseView : UIView{
         currentScore.center = CGPointMake(CGRectGetMaxX(self.frame) * 0.30,CGRectGetMaxY(self.frame) * 0.75)
         currentScore.textAlignment = .Center
         
-//        lifetimeScoreLabel.frame = CGRectMake(CGRectGetMidX(rect), CGRectGetMidY(rect), 200, 50)
-//        lifetimeScoreLabel.text = "Lifetime Score"
-//        lifetimeScoreLabel.adjustsFontSizeToFitWidth = true
-//        lifetimeScoreLabel.center = CGPointMake(CGRectGetMaxX(self.frame) * 0.75,CGRectGetMaxY(self.frame) * 0.70)
-//        lifetimeScoreLabel.textAlignment = .Center
         lifetimeScoreImageBackground.frame = CGRectMake(CGRectGetMidX(rect), CGRectGetMidY(rect), 200, 50)
         lifetimeScoreImageBackground.image = lifetimeScoreImage
         self.addSubview(lifetimeScoreImageBackground)
@@ -96,10 +91,9 @@ class PauseView : UIView{
         lifetimeScore.center = CGPointMake(CGRectGetMaxX(self.frame) * 0.75,CGRectGetMaxY(self.frame) * 0.75)
         lifetimeScore.textAlignment = .Center
         
-//        self.addSubview(musicLabel)
-//        self.addSubview(fxLabel)
-//        self.addSubview(lifetimeScoreLabel)
-//        self.addSubview(currentScoreLabel)
+        self.addSubview(musicSlider!)
+        self.addSubview(fxSlider!)
+        
         self.addSubview(currentScore)
         self.addSubview(lifetimeScore)
         
